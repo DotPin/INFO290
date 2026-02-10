97aaf885ad26a50660b57b1326041a729c4b9da6
dotpin <d.rojas.asenjo@gmail.com>
2026-02-09 21:03:42 -0300

    Fix octave code

--- Metodos Numericos/Trabajo 2017-1/Solucion/Definitivo.m
+++ Metodos Numericos/Trabajo 2017-1/Solucion/Definitivo.m
@@ -12,43 +12,42 @@ format short
 plp = true;
   while plp
     nn = input("Ingrese el numero de error posible a iterar, debe ser menor que 0.05: ");
-    if nn <0.05
+    if nn <0.05;
       plp = false;
     end
   end
 
-  
+
 a = input("Ingrese el lado cuadrado de la superficie: ");
 z = input("Ingrese la profundidad de la superficie: ");
 a = a*2-2
 z = z*2-2
-  
+
 % Matriz D
 D=zeros(n,n);
-for i=1:1:n
+for i=1:1:n;
     D(i,i)=A(i,i);
-           
 end
 
 % Matriz U
 F=zeros(n,n);
-for c=1:1:n
-    for f=c+1:1:n
+for c=1:1:n;
+    for f=c+1:1:n;
         F(c,f) = A(c,f);
     end
 end
 
 % Matriz L
 E = zeros(n,n);
-for c=1:1:n
-    for f=c+1:1:n
+for c=1:1:n;
+    for f=c+1:1:n;
         E(f,c) = A(f,c);
     end
 end
 
 % Matriz Mgs y Vgs
-Mgs=-inv(D+E)*F
-Vgs= inv(D+E).*B' 
+Mgs=-inv(D+E)*F;
+Vgs= inv(D+E).*B';
 
 % Iteraciones para solucion del sistema
 x=zeros(n,1);
@@ -57,15 +56,15 @@ norm((Mgs*x+Vgs - x),2);
 printf ("Numero de iteraciones aproximadas a realizar")
 k = ceil( log((nn*(1-norm(Mgs,2)))/norm((Mgs*x+Vgs - x),2))/log(norm(Mgs,2)) )
 
-zz = input("Comienza el metodo iterativo, presione 'enter' ")
+zz = input("Comienza el metodo iterativo, presione 'enter' ");
 
-for i=0:1:k
+for i=0:1:k;
       v = x;
-      x=Mgs*x+Vgs
+      x=Mgs*x+Vgs;
       x1 = x-v;
       e = norm(x1,2)/norm(x,2);
       if e < nn
-	break
+	      break
       end
 end
 printf("Solucion aproximada 'X' del sistema \n")
@@ -78,27 +77,29 @@ i
 matriz = zeros(a,a,z);
 n
 p = 0
-for i=1:z
-  for j=1:a
-    for k=1:a
-      p += 1
+for i=1:z;
+  for j=1:a;
+    for k=1:a;
+      p += 1;
       matriz(k,j,i)= x(p)*-1;
     end
   end
 end
 
 
-printf("A continuación se mostrarán los valores representados en cada malla como conjunto solución de los nodos \n");
-for i=1:z
-  matriz(:,:,i)
-end
+%printf("A continuación se mostrarán los valores representados en cada malla como conjunto solución de los nodos \n");
+%for i=1:z
+  %matriz(:,:,i);
+%end
+
 disp('Matriz resuelta por Diferencias Finitas:');
 %disp(matriz);
 figure(2);
-mesh(matriz);
+[x,y,z] = ind2sub(size(matriz), find(matriz > 0.00001));
+scatter3(x, y, z, 30, matriz(matriz > 0.00001), 'filled')
+colorbar
 title("Gráfico de Tº de una placa irregular 3D");
 xlabel('Nodo posición y');
 ylabel('Nodo posición x');
 zlabel('Temperatura de la placa');
-printf("Exportando resultados en archivo DF_X.csv \n");
 xxx = input("Presione enter para terminar");
